54505fdce93ebcc654fcdff0a12d905f81b2da14
Rupe88 <chyrupesh828@gmail.com>
2024-06-22 12:19:43 +0545

    global catchAsynceError handler created

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import User from "../database/models/userModel";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 class AuthController {
- public static async registerUser(req: Request, res: Response): Promise<void> {
-    const { username, email, password } = req.body;
+  public static async registerUser(req: Request, res: Response): Promise<void> {
+    const { username, email, password, role } = req.body;
     if (!username || !email || !password) {
       res.status(400).json({
         message: "please provide username, email, password",
@@ -14,12 +15,56 @@ class AuthController {
     await User.create({
       username,
       email,
-      password:bcrypt.hashSync(password, 10),
+      password: bcrypt.hashSync(password, 10),
+      role:role
     });
     res.status(200).json({
       success: true,
       message: "user registered successfully",
     });
+  }
+
+  //login
+
+  public static async loginUser(req: Request, res: Response): Promise<void> {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400).json({
+        message: "please provide email, password",
+      });
+      return;
+    }
+
+    //check user exists || !
+    const [data] = await User.findAll({
+      where: {
+        email: email,
+      },
+    });
+    if (!data) {
+      res.status(404).json({
+        message: "no data with that email",
+      });
+      return;
+    }
+
+    //check password || compare
+    const isMatched = bcrypt.compareSync(password, data.password);
+    if (isMatched) {
+      //generate token
+     const token= jwt.sign({ id: data.id }, "hahaha", {
+        expiresIn: "20d",
+      });
+      res.status(200).json({
+        token,
+        message:"logged in Success"
+
+      })
+    } else {
+      res.status(403).json({
+        message: "Invalid email or password",
+      });
+    }
   }
 }
 export default AuthController;
--- src/database/models/userModel.ts
+++ src/database/models/userModel.ts
@@ -21,6 +21,12 @@ class User extends Model{
     declare username:string
 
     @Column({
+        type:DataType.ENUM("customer", "admin"),
+        defaultValue:"customer"
+    })
+    declare role:string
+
+    @Column({
         type:DataType.STRING
     })
     declare email:string
