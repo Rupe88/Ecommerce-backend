56d3b53455bd83982e352e4e176e1da92f65b906
Rupe88 <chyrupesh828@gmail.com>
2024-06-26 18:04:32 +0545

    payment model created

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -3,41 +3,52 @@ import User from "./models/userModel";
 import Product from "./models/productModel";
 import Category from "./models/categoryModel";
 import Cart from "./models/cartModel";
+import Order from "./models/orderModel";
+import OrderDetail from "./models/orderDetailsModel";
 const sequelize = new Sequelize({
   database: process.env.DB_NAME,
   dialect: "mysql",
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT),
-  models:[__dirname + "/models"]
+  models: [__dirname + "/models"],
 });
 
-sequelize.authenticate()
-.then(()=>{
-    console.log("database is connected")
-}).catch((error)=>{
-    console.log("error in db connection", error)
-})
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("database is connected");
+  })
+  .catch((error) => {
+    console.log("error in db connection", error);
+  });
 
-sequelize.sync({force:false}).then(()=>{
-    console.log("migrated || sync")
-}).catch((error)=>{
-console.log("error in db migration || sync", error)
-})
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("migrated || sync");
+  })
+  .catch((error) => {
+    console.log("error in db migration || sync", error);
+  });
 
 //relationships
-User.hasMany(Product,{foreignKey:"userId"})
-Product.belongsTo(User, {foreignKey:"userId"})
+User.hasMany(Product, { foreignKey: "userId" });
+Product.belongsTo(User, { foreignKey: "userId" });
+
+Product.belongsTo(Category, { foreignKey: "categoryId" });
+Category.hasOne(Product, { foreignKey: "categoryId" });
+
+User.hasMany(Cart, { foreignKey: "userId" });
+Cart.belongsTo(User, { foreignKey: "userId" });
+
+Product.hasMany(Cart, { foreignKey: "productId" });
+Cart.belongsTo(Product, { foreignKey: "productId" });
+
+//order details relation
+Order.hasMany(OrderDetail, { foreignKey: "orderId" });
+OrderDetail.belongsTo(Order, { foreignKey: "orderId" });
 
 
-Product.belongsTo(Category, {foreignKey:"categoryId"})
-Category.hasOne(Product,{foreignKey:"categoryId"})
-
-User.hasMany(Cart, {foreignKey:"userId"})
-Cart.belongsTo(User, {foreignKey:"userId"})
-
-
-Product.hasMany(Cart, {foreignKey:"productId"})
-Cart.belongsTo(Product, {foreignKey:"productId"})
 
 export default sequelize;
--- src/database/models/paymentModel.ts
+++ src/database/models/paymentModel.ts
@@ -2,11 +2,11 @@ import { UUIDV4 } from "sequelize";
 import { Table, Column, DataType, Model } from "sequelize-typescript";
 
 @Table({
-  tableName: "orders",
-  modelName: "Order",
+  tableName: "payments",
+  modelName: "Payment",
   timestamps: true,
 })
-class Order extends Model {
+class Payment extends Model {
   @Column({
     primaryKey: true,
     type: DataType.UUID,
@@ -33,4 +33,4 @@ class Order extends Model {
   
 }
 
-export default Order;
+export default Payment;
